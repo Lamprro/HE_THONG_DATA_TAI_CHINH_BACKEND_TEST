be20262b0de9d01b8fc3f74bf8eebf7fa08d28ad
maiva <maivannam1102005@gmail.com>
2026-09-19 15:10:02 +0700

    fix: synchronize market repositories and restore build

--- src/main/java/com/hethongdata/taichinh/config/PersistenceConfiguration.java
+++ src/main/java/com/hethongdata/taichinh/config/PersistenceConfiguration.java
@@ -15,25 +15,8 @@ import com.hethongdata.taichinh.entity.master.SecurityEntity;
 import com.hethongdata.taichinh.entity.validation.DataVersionEntity;
 import com.hethongdata.taichinh.entity.validation.ValidationResultEntity;
 import com.hethongdata.taichinh.entity.validation.ValidationRuleEntity;
-import com.hethongdata.taichinh.repository.jpa.ingestion.DataSourceJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.news.NewsArticleCompanyJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.news.NewsArticleJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.IndexPriceJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.MarketIndexJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.SecurityIndexMembershipJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.ingestion.IngestionJobJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.ingestion.IngestionRunJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.ingestion.RawPayloadJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.master.CompanyAliasJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.master.CompanyJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.master.SecurityJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.validation.DataVersionJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.validation.ValidationResultJpaRepository;
-import com.hethongdata.taichinh.repository.jpa.validation.ValidationRuleJpaRepository;
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Registers the ingestion, master data and validation persistence packages used by the application.
@@ -48,16 +31,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
             CompanyEntity.class, CompanyAliasEntity.class, SecurityEntity.class,
             NewsArticleEntity.class, NewsArticleCompanyEntity.class,
             MarketIndexEntity.class, IndexPriceEntity.class, SecurityIndexMembershipEntity.class
-        })
-@EnableJpaRepositories(
-        basePackageClasses = {
-            DataSourceJpaRepository.class, IngestionJobJpaRepository.class,
-                    IngestionRunJpaRepository.class, RawPayloadJpaRepository.class,
-            ValidationRuleJpaRepository.class, ValidationResultJpaRepository.class,
-                    DataVersionJpaRepository.class,
-            CompanyJpaRepository.class, CompanyAliasJpaRepository.class, SecurityJpaRepository.class,
-            NewsArticleJpaRepository.class, NewsArticleCompanyJpaRepository.class,
-            MarketIndexJpaRepository.class, IndexPriceJpaRepository.class,
-            SecurityIndexMembershipJpaRepository.class
         })
 public class PersistenceConfiguration {}
